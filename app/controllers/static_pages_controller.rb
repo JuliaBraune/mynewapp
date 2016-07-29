0933aefa1bc72a3a35d9d43f1cb229c73c2a423d
JuliaBraune <julia.braune@gmx.net>
2016-07-29 22:34:54 +0200

    task 5.3 added contact_form, added plugin sengrid for heroku, generated Mailer, styled email template, styled contact-page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,6 +4,16 @@ class StaticPagesController < ApplicationController
   def landing_page
   	@products = Product.limit(2)
 	end
+	def thank_you
+		@name=params[:name]
+		@email=params[:email]
+		@message=params[:message]
+		UserMailer.contact_form(@email, @name, @message).deliver_now
+		ActionMailer::Base.mail(:from => @email, 
+			:to => 'julia.braune@gmx.net', 
+			:subject => "An new message from #{@name}", 
+			:body => @message).deliver_now
+	end
 end
 	
 
--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'julia.braune@gmx.net'
   layout 'mailer'
 end
