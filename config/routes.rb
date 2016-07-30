d6ff8159184e916d463fb58e5d2e5907985b0715
JuliaBraune <julia.braune@gmx.net>
2016-07-30 15:14:51 +0200

    deleted some misspelling in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users, :path => '', :path_names => {:sing_in => 'login', :sign_out => 'logout'}
+
+  devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout'}
+  
   resources :users
+
   resources :products
+
   get 'static_pages/about'
 
   get 'static_pages/contact'
@@ -13,6 +17,7 @@ Rails.application.routes.draw do
   post 'static_pages/thank_you'
 
   resources :orders, only: [:index, :show, :create, :destroy]
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
