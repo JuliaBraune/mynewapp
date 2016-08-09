767c55abc9ee3158a2571543e0fe72d41e6c7a34
JuliaBraune <julia.braune@gmx.net>
2016-08-09 23:48:49 +0200

    task 5.8 added comments controller and star rating system

---
+++ app/assets/javascripts/site.js
@@ -0,0 +1,10 @@
+$(document).on('turbolinks:load', function(){ 
+	$('rating').raty( { path: '/assets', scoreName: 'comment[rating]' }); //specified where the image assets are found
+});
+	$('.rated').raty( {path: '/assets',
+		readOnly: true,
+		score: function() {
+			return $(this).attr('data-score');
+		}
+	});
+});
