6c67279ce8fd8f3d672eb50c348de4994b0174ac
JuliaBraune <julia.braune@gmx.net>
2016-08-09 13:02:04 +0200

    task5.7 added comment-model, styled product-show-page

--- config/routes.rb
+++ config/routes.rb
@@ -6,17 +6,17 @@ Rails.application.routes.draw do
 
   resources :products
 
+  resources :orders, only: [:index, :show, :create, :destroy]
+
   get 'static_pages/about'
 
   get 'static_pages/contact'
 
   get 'static_pages/index'
 
-  root 'static_pages#landing_page'
-
   post 'static_pages/thank_you'
 
-  resources :orders, only: [:index, :show, :create, :destroy]
+  root 'static_pages#landing_page'
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
