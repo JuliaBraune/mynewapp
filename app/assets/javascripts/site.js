31977e86dfdbf3b00f9f81c88deb373d5a6092f1
JuliaBraune <julia.braune@gmx.net>
2016-08-10 14:17:58 +0200

    task 5.9. added pagination for comments; styled comments

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,5 +1,5 @@
 $(document).on('turbolinks:load', function(){ 
-	$('rating').raty( { path: '/assets', scoreName: 'comment[rating]' }); //specified where the image assets are found
+	$('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' }); //specified where the image assets are found
 	$('.rated').raty( {path: '/assets',
 		readOnly: true,
 		score: function() {
