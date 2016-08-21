38f7f7442d0396f27edc3ef8d29ba705cdedbfc9
JuliaBraune <julia.braune@gmx.net>
2016-08-21 14:57:50 +0200

    tried to solve some issues – image_tag

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -1,4 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
 # Configure sensitive parameters which will be filtered from the log file.
-Rails.application.config.filter_parameters += [:password]
+# Rails.application.config.filter_parameters += [:password]
+
+# tells Rails not to put passwords in the log files
+Rails.application.config.filter_parameters << :password
